d0045d4e25605260f49c36a089ab2c3e499e682e
Xeonoff <72578165+Xeonoff@users.noreply.github.com>
2025-03-06 22:14:31 +0300

    верстка главной страницы с чатами на исправления

---
+++ server/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import express from "express";
+import morgan from "morgan"
+import path from "path";
+
+const app = express();
+const port = 8081;
+const hostname = 'localhost';
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.static("images"));
+
+app.get('/login', (req, res) => {
+    res.send('Hello World')
+});
+  
+app.get('/signup', (req, res) => {
+  res.send('Hello World!!!')
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server is running on http://${hostname}:${port}/`);
+});

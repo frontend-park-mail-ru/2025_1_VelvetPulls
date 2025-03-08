9e319b1e59ca59ee4db08b5d4fbe143680c3efe8
ruslan <runovikov2@gmail.com>
2025-03-08 23:01:12 +0300

    added routing

--- server/server.js
+++ server/server.js
@@ -1,23 +1,42 @@
 import express from "express";
-import morgan from "morgan"
+import morgan from "morgan";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Получаем текущий путь к файлу (ES-модули)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 8081;
 const hostname = 'localhost';
 
+
+// Указываем путь к папке public
+const publicPath = path.join(__dirname, "..", "public");
+
+
+// Middleware для статических файлов
+app.use(express.static(publicPath));
+
+
+// Включаем логирование
 app.use(morgan("dev"));
-app.use(express.static("public"));
-app.use(express.static("images"));
 
-app.post('/login', (req, res) => {
-    res.send('Hello World')
-});
-  
-app.post('/signup', (req, res) => {
-  res.send('Hello World!!!')
+// Роутинг
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicPath, "main_page.html"));
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(publicPath, "login.html"));
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(publicPath, "signup.html"));
+});
+
+// Запуск сервера
 app.listen(port, hostname, () => {
   console.log(`Server is running on http://${hostname}:${port}/`);
 });

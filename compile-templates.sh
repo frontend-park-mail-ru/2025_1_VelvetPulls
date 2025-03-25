f2df82852a3906542fef8665f80d3e1604337036
ruslan <runovikov2@gmail.com>
2025-03-26 00:13:42 +0300

    убрать вывод компилируемых шаблонов

--- compile-templates.sh
+++ compile-templates.sh
@@ -2,6 +2,5 @@
 
 find . -type f -name "*.hbs" | while read -r template; do
     precompiled_file="${template%.hbs}.precompiled.js"
-    echo $template $precompiled_file
     handlebars $template -f $precompiled_file
 done

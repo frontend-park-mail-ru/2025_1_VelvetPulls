ba754f2b6f78a91762e859e637aa2d44d908ab9d
ruslan <runovikov2@gmail.com>
2025-03-26 00:08:13 +0300

    компиляция шаблонов по регулярному выражению

--- compile-templates.sh
+++ compile-templates.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-handlebars public/pages/login/login.hbs -f public/pages/login/login.precompiled.js
-handlebars public/pages/signup/signup.hbs -f public/pages/signup/signup.precompiled.js
-handlebars public/pages/mainPage/mainPage.hbs -f public/pages/mainPage/mainPage.precompiled.js
-handlebars public/components/AuthForm/AuthForm.hbs -f public/components/AuthForm/AuthForm.precompiled.js
-handlebars public/components/PopOver/PopOver.hbs -f public/components/PopOver/PopOver.precompiled.js
-handlebars public/components/ListOfChats/ListOfChats.hbs -f public/components/ListOfChats/ListOfChats.precompiled.js
-handlebars public/components/Profile/Profile.hbs -f public/components/Profile/Profile.precompiled.js
-handlebars public/components/Contacts/Contacts.hbs -f public/components/Contacts/Contacts.precompiled.js
+
+find . -type f -name "*.hbs" | while read -r template; do
+    precompiled_file="${template%.hbs}.precompiled.js"
+    echo $template $precompiled_file
+    handlebars $template -f $precompiled_file
+done

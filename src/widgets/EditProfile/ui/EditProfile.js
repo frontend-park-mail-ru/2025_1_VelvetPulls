f5094beb30680ca0901e9c1e6dec0439c354d99f
ruslan <runovikov2@gmail.com>
2025-04-18 17:24:08 +0300

    сделать загрузку аватара в окне для редактирования профиля

--- src/widgets/EditProfile/ui/EditProfile.js
+++ src/widgets/EditProfile/ui/EditProfile.js
@@ -10,6 +10,7 @@ class EditProfile {
             lastName: currentUser.getLastName(),
             email: currentUser.getEmail(),
             username: currentUser.getUsername(),
+            avatarSrc: currentUser.avatarSrc,
         };
 
         const editProfileTemplate = Handlebars.templates["EditProfile.hbs"];
@@ -18,27 +19,27 @@ class EditProfile {
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, "text/html");
 
-        const fileInput1=doc.querySelector("#fileInput1")
-        const avatarContainer = doc.querySelector('.avatar-container-upload');
-    const avatarImage = doc.getElementById('avatarImage');
+        const fileInput1 = doc.querySelector("#fileInput1");
+        const avatarContainer = doc.querySelector(".avatar-container-upload");
+        const avatarImage = doc.getElementById("avatarImage");
 
-    avatarContainer.addEventListener('click', function() {
-      fileInput1.click(); // Открываем диалог выбора файла
-    });
+        avatarContainer.addEventListener("click", function () {
+            fileInput1.click(); // Открываем диалог выбора файла
+        });
 
-    fileInput1.addEventListener('change', function(event) {
-      const file = event.target.files[0];
-      this.avatar=file
-      if (file) {
-        const reader = new FileReader();
+        fileInput1.addEventListener("change", function (event) {
+            const file = event.target.files[0];
+            this.avatar = file;
+            if (file) {
+                const reader = new FileReader();
 
-        reader.onload = function(e) {
-          avatarImage.src = e.target.result; // Устанавливаем выбранное фото
-        }
+                reader.onload = function (e) {
+                    avatarImage.src = e.target.result; // Устанавливаем выбранное фото
+                };
 
-        reader.readAsDataURL(file); // Читаем файл как Data URL
-      }
-    });
+                reader.readAsDataURL(file); // Читаем файл как Data URL
+            }
+        });
 
         const container = doc.body.firstChild;
         this.container = container;
@@ -64,35 +65,40 @@ class EditProfile {
             this.updateUser();
         });
     }
+
     isEqualPasswords(input1, input2) {
         return input1 === input2;
     }
+
     createError(input, text) {
         const parent = input.parentNode;
         parent.classList.add("auth-form__error");
-    
+
         const errorLabel = document.createElement("label");
         errorLabel.classList.add("error-label");
         errorLabel.textContent = text;
-    
+
         parent.append(errorLabel);
     }
+
     removeError(input) {
         const parent = input.parentNode;
-    
+
         if (parent.classList.contains("auth-form__error")) {
             parent.querySelector(".error-label").remove();
             parent.classList.remove("auth-form__error");
         }
     }
+
     async updateUser() {
-        const avaImg=this.container.querySelector("#fileInput1")
+        const avaImg = this.container.querySelector("#fileInput1");
         // let avatarFile =
         //     avatarInput.files.length > 0 ? avatarInput.files[0] : null;
-        const  avatarFile=avaImg.files.length > 0 ? avaImg.files[0] : null;
-        console.log(avatarFile)
+        const avatarFile = avaImg.files.length > 0 ? avaImg.files[0] : null;
+        console.log(avatarFile);
 
-        const firstNameInput = this.container.querySelector("#first-name-input");
+        const firstNameInput =
+            this.container.querySelector("#first-name-input");
         const firstName = firstNameInput.value;
 
         const lastNameInput = this.container.querySelector("#last-name-input");
@@ -104,12 +110,16 @@ class EditProfile {
         const usernameInput = this.container.querySelector("#username-input");
         const username = usernameInput.value;
 
-        const newPasswordInput = this.container.querySelector("#new-password-input");
+        const newPasswordInput = this.container.querySelector(
+            "#new-password-input",
+        );
         const newPassword = newPasswordInput.value;
 
-        const repeatPasswordInput = this.container.querySelector("#repeat-password-input");
+        const repeatPasswordInput = this.container.querySelector(
+            "#repeat-password-input",
+        );
         const repeatPassword = repeatPasswordInput.value;
-        if (this.isEqualPasswords(newPassword, repeatPassword)){
+        if (this.isEqualPasswords(newPassword, repeatPassword)) {
             const profileData = {
                 first_name: firstName,
                 last_name: lastName,
@@ -130,12 +140,13 @@ class EditProfile {
             await currentUser.init(null);
 
             eventBus.emit("edit profile -> save");
-        }
-        else if (newPassword != null){
+        } else if (newPassword != null) {
             this.removeError(repeatPasswordInput);
-            this.createError(repeatPasswordInput, "Новый пароль не подтвержден!");
-        }
-        else{
+            this.createError(
+                repeatPasswordInput,
+                "Новый пароль не подтвержден!",
+            );
+        } else {
             const profileData = {
                 first_name: firstName,
                 last_name: lastName,
@@ -149,7 +160,6 @@ class EditProfile {
             if (avatarFile !== null) {
                 formData.append("avatar", avatarFile);
             }
-
 
             formData.append("profile_data", JSON.stringify(profileData));
 

3107fe0e7a0c30320bc8b8347ac7d13c0bdb5a91
Xeonoff <72578165+Xeonoff@users.noreply.github.com>
2025-05-06 15:05:41 +0300

    channel follower input

--- src/widgets/Contacts/ui/Contacts.js
+++ src/widgets/Contacts/ui/Contacts.js
@@ -35,7 +35,7 @@ class Contacts {
                 // console.log(finder.value)
                 const responseBody1 = await api.get(`/search/contacts?query=${finder.value}`);
                 //console.log(responseBody1.data)
-                const searchEmpty = false;
+                let searchEmpty = false;
                 const contacts = [];
                 if (responseBody1.data !== null) {
                     for (const contact of responseBody1.data) {

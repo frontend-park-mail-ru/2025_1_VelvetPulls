30ef2f35ba13b53dd675467915004ae3c4edfcda
vden5 <voronov.denis.2005@mail.ru>
2025-05-09 23:55:50 +0300

    add chats page

---
+++ source/widgets/Chat/api/SendMessage.ts
@@ -0,0 +1,22 @@
+import { API } from "@/shared/api/api";
+import { EmptyResponse, SendMessageRequest } from "@/shared/api/types";
+
+export const SendMessage = async (chatId : string, text : string, files? : File[], photos? : File[]) => {
+    const formData = new FormData();
+    formData.append("text", JSON.stringify(text));
+    files?.map((file) => {
+        formData.append('files', file);
+    })
+    photos?.map((photo) => {
+        formData.append('photos', photo);
+    })
+    console.log(formData.get("text"))
+
+    const response = await API.postFormData<SendMessageRequest>(
+        `/chat/${chatId}/messages`,
+        formData,
+      );
+    console.log(response)
+
+    return response;
+};

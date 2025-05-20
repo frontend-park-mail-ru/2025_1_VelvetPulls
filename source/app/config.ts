0507863fc3aa0e21d738dc1f0dd084bb30364dff
Xeonoff <72578165+Xeonoff@users.noreply.github.com>
2025-05-20 18:40:32 +0300

    init deploy branch

--- source/app/config.ts
+++ source/app/config.ts
@@ -4,6 +4,6 @@ export const ChatLabel = {
   channel: "Канал",
 };
 
-export const serverHost = "http://localhost:8080/api";
-export const staticHost = "http://localhost:8080/";
-export const websocketHost = "ws://localhost:8082";
+export const serverHost = "http://90.156.217.108:8080/api";
+export const staticHost = "http://90.156.217.108:8080/";
+export const websocketHost = "ws://90.156.217.108:8082";
